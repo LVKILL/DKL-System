a274c4c4b7b456acd18571f5294f5171fb985560
LVKILL <lvkill@gmail.com>
2019-10-23 10:22:55 -0300

    Sincronização inicial do projeto Spring start criado nesta data

---
+++ DKL-System/src/test/java/com/example/dklsys/DklSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dklsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DklSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
