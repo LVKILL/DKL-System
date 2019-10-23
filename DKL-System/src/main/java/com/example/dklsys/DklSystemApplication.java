20fd4e515b795382c2388e3d7f1e06e4896a9269
LVKILL <lvkill@gmail.com>
2019-10-23 10:55:22 -0300

    Adicionada estrutura inicial do Spring Tools

---
+++ DKL-System/src/main/java/com/example/dklsys/DklSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.dklsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DklSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DklSystemApplication.class, args);
+	}
+
+}
